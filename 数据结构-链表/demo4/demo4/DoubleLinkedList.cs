caf3aa6437da94885169a548873ad6861dd78888
zc282840325 <282840325@qq.com>
2019-11-17 17:31:17 +0800

    双向链表的增、删、改、查

---
+++ 数据结构-链表/demo4/demo4/DoubleLinkedList.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4
+{
+   public class DoubleLinkedList
+    {
+        //初始化一个头结点，头结点不要动，不存放具体的数据
+        private HeroNode head = new HeroNode(0, "", "");
+
+        //返回头结点
+        public HeroNode gethead()
+        {
+            return head;
+        }
+        //显示链表
+        public void showLink()
+        {
+            if (head.next == null)
+            {
+                Console.WriteLine("链表为空！");
+            }
+            //因为头结点不能懂，我们需要一个辅助变量来遍历
+            HeroNode temp = head.next;
+            while (true)
+            {
+                //找到链表的最后
+                if (temp.next == null)
+                {
+                    Console.WriteLine(temp);
+                    break;
+                }
+                Console.WriteLine(temp);
+                //将temp后移
+                temp = temp.next;
+            }
+        }
+        //添加节点到单向链表
+        public void add(HeroNode heroNode)
+        {
+            //因为head节点不能动，因此我们需要一个辅助遍历temp
+            HeroNode temp = head;
+            //遍历链表，找到最后
+            while (true)
+            {
+                //找到链表的最后
+                if (temp.next == null)
+                {
+                    break;
+                }
+                //如果没有找到最后
+                temp = temp.next;
+            }
+            //当退出while循环时，temp就指向了链表的最后
+            temp.next = heroNode;
+            heroNode.pre = temp;
+        }
+        //按照编号添加
+        public void addByOrder(HeroNode heroNode)
+        {
+            HeroNode temp = head;
+            bool flag = false;//标志添加的编号是否存在，默认为false
+            while (true)
+            {
+                if (temp.next == null)//说明temp已经在链表的最后
+                {
+                    break;
+                }
+                if (temp.next.no > heroNode.no)//位置找到，就在temp后面插入
+                {
+                    break;
+
+                }
+                else if (temp.next.no == heroNode.no)
+                {
+                    flag = true;//说明编号存在
+                    break;
+                }
+                temp = temp.next;
+            }
+            //判断flag的值，如果flag==true,说明编号存在
+            if (flag)
+            {
+                Console.WriteLine("准备插入的英雄的编号{0}已经存在了，不能加入\n", heroNode.no);
+            }
+            else
+            {
+                //插入到temp后面
+                temp.next.pre = heroNode;
+                heroNode.next = temp.next;
+
+                temp.next = heroNode;
+                heroNode.pre = temp;
+            }
+        }
+        //修改结点的信息，根据编号no来修改，即no编号不能改
+        public void update(HeroNode newheroNode)
+        {
+            if (head.next == null)
+            {
+                Console.WriteLine("链表为空！");
+            }
+            else
+            {
+                HeroNode temp = head;
+                bool flag = false;//是否找到该结点
+                while (true)
+                {
+                    if (temp == null)
+                    {
+                        break;
+                    }
+                    if (temp.no == newheroNode.no)
+                    {
+                        //找到
+                        flag = true;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (flag)
+                {
+                    temp.name = newheroNode.name;
+                    temp.nickname = newheroNode.nickname;
+                }
+                else
+                {
+                    Console.WriteLine("没有找到编号{0}的结点，不能修改\n", newheroNode.no);
+                }
+            }
+        }
+        //删除结点
+        public void delete(int n)
+        {
+            if (head.next == null)
+            {
+                Console.WriteLine("链表为空！");
+            }
+            else
+            {
+                HeroNode temp = head;
+                bool flag = false;//是否找到该结点
+                while (true)
+                {
+                    if (temp == null)
+                    {
+                        break;
+                    }
+                    if (temp.no == n)
+                    {
+                        //找到
+                        flag = true;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (flag)
+                {
+                    temp.pre.next = temp.next;
+                    if (temp.next!=null)
+                    {
+                        temp.next.pre = temp.pre;
+
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("没有找到编号{0}的结点，不能修改\n", n);
+                }
+            }
+        }
+    }
+
+    public class HeroNode
+    {
+        public int no;
+        public string name;
+        public string nickname;
+        public HeroNode next;//指向下一个节点
+        public HeroNode pre;//指向下一个节点
+        //构造器
+        public HeroNode(int no, string name, string nickname)
+        {
+            this.no = no;
+            this.name = name;
+            this.nickname = nickname;
+        }
+        //为了现实方法，我们重新toString
+        public override string ToString()
+        {
+            return "HeroNode[no=" + no + ",name=" + name + ",nickname=" + nickname + "]";
+        }
+    }
+}
---
+++ 数据结构-链表/demo4/demo4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("双向链表的测试：");
+            HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
+            HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
+            HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
+            HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
+            DoubleLinkedList linkedList = new DoubleLinkedList();
+            Console.WriteLine("添加元素：");
+            linkedList.add(hero1);
+            linkedList.add(hero3);
+            linkedList.add(hero4);
+
+            Console.WriteLine("打印：");
+            linkedList.add(hero2);
+            HeroNode hero5 = new HeroNode(3, "吴用2", "智多星2");
+            linkedList.update(hero5);
+            linkedList.showLink();
+            Console.WriteLine("修改后的：");
+            linkedList.delete(2);
+            Console.WriteLine("删除2之后的：");
+            linkedList.showLink();
+            HeroNode hero6 = new HeroNode(2, "卢俊义", "玉麒麟");
+            linkedList.addByOrder(hero6);
+            Console.WriteLine("添加一个2");
+            linkedList.showLink();
+            Console.ReadKey();
+        }
+    }
+}
