f1d53d2e7ebe167adf05e5092faf167882d02614
zc282840325 <282840325@qq.com>
2019-11-19 16:56:59 +0800

    逆波兰计算

---
+++ 数据结构-逆波兰时计算器/demo/demo/PolandNotation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace demo
+{
+   public class PolandNotation
+    {
+        public List<string> getListString(string suff)
+        {
+            string[] split = suff.Split(' ');
+            List<string> list = new List<string>();
+            foreach (var item in split)
+            {
+                list.Add(item);
+            }
+            return list;
+        }
+        public int calculate(List<string> list)
+        {
+            Stack<string> stack = new Stack<string>();
+            foreach (var item in list)
+            {
+                Regex regExp = new Regex("^\\d+$");//匹配的是多位数
+               
+                if (regExp.IsMatch(item))
+                {
+                    //入栈
+                    stack.Push(item);
+                }
+                else
+                {
+                    int num2 = Convert.ToInt32(stack.Pop());
+                    int num1 = Convert.ToInt32(stack.Pop());
+                    int res = 0;
+                    if (item=="+")
+                    {
+                        res = num1 + num2;
+                    }
+                    else if (item == "-")
+                    {
+                         res = num1 - num2;
+                    }
+                    else if (item == "*")
+                    {
+                        res = num1 * num2;
+                    }
+                    else if (item == "/")
+                    {
+                        res = num1 / num2;
+                    }
+                    else
+                    {
+                        throw new Exception("运算符有问题！ ");
+                    }
+                    //把res入栈
+                    stack.Push(""+res);
+                }
+            }
+
+            return Convert.ToInt32(stack.Pop());
+        }
+    }
+
+}
---
+++ 数据结构-逆波兰时计算器/demo/demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "(30+4)*5-6";
+            string str2 = "4*5-8+60+8/2";
+
+            string suffixExpression = "30 4 + 5 * 6 -";
+            suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
+            PolandNotation poland = new PolandNotation();
+            List<string> list = poland.getListString(suffixExpression);
+            Console.WriteLine(poland.calculate(list));
+        }
+    }
+}
