2c8bcd507fa4111f1ec1f025d2b893a6f351bcf3
zc282840325 <282840325@qq.com>
2019-11-24 23:29:04 +0800

    希尔

---
+++ 数据结构-希尔排序/demo/demo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {8,9,1,7,2,3,5,4,6,0 };
+            shellSort2(arr);
+        }
+
+        public static void shellSort(int[] arr)
+        {
+            //希尔排序的第一论
+            //因为第一轮排序，是将10个数据分成5组，
+            int temp = 0;
+            int len = arr.Length;
+            int count = 0;
+            //将数组除2取分组的个数
+            for (int num = len / 2; num >0; num/= 2)
+            {
+                //组队的值进行比较,满足条件就交换
+                for (int i = num; i < arr.Length; i++)
+                {
+                    for (int j = i - num; j >= 0; j -= num)
+                    {
+                        //满足条件
+                        if (arr[j] > arr[j + num])
+                        {
+                            //组内交换
+                            temp = arr[j];
+                            arr[j] = arr[j + num];
+                            arr[j + num] = temp;
+                        }
+                    }
+                }
+                count++;
+                Console.WriteLine("这是第{0}次排序", count);
+                print(arr);
+            }
+
+        }
+        public static void shellSort2(int[] arr)
+        {
+            //希尔排序的第一论
+            //因为第一轮排序，是将10个数据分成5组，
+            int temp = 0;
+            int len = arr.Length;
+            int count = 0;
+            //将数组除2取分组的个数
+            for (int num = len / 2; num > 0; num /= 2)
+            {
+                //组队的值进行比较,满足条件就交换
+                for (int i = num; i < arr.Length; i++)
+                {
+                    int j = i;
+                    temp = arr[j];
+                    if (arr[j]<arr[j-num])
+                    {
+                        //移位
+                        while (j-num>=0&& temp<arr[j-num])
+                        {
+                            arr[j] = arr[j - num];
+                            j -= num;
+                        }
+                        arr[j] = temp;
+                    }
+                }
+                count++;
+                Console.WriteLine("这是第{0}次排序", count);
+                print(arr);
+            }
+        }
+
+        public static void print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
