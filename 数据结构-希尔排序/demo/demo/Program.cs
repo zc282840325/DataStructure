f2f27e0732a5c5c10e5efd4446f7580f753b4cfe
zc282840325 <282840325@qq.com>
2019-11-25 10:04:52 +0800

    快速排序

--- 数据结构-希尔排序/demo/demo/Program.cs
+++ 数据结构-希尔排序/demo/demo/Program.cs
@@ -11,9 +11,13 @@ namespace demo
         static void Main(string[] args)
         {
             int[] arr = {8,9,1,7,2,3,5,4,6,0 };
-            shellSort2(arr);
+            test2(arr);
+            print(arr);
         }
-
+        /// <summary>
+        ///  希尔换位法
+        /// </summary>
+        /// <param name="arr"></param>
         public static void shellSort(int[] arr)
         {
             //希尔排序的第一论
@@ -45,6 +49,10 @@ namespace demo
             }
 
         }
+        /// <summary>
+        /// 希尔移位法
+        /// </summary>
+        /// <param name="arr"></param>
         public static void shellSort2(int[] arr)
         {
             //希尔排序的第一论
@@ -84,6 +92,50 @@ namespace demo
                 Console.Write(arr[i] + "\t");
             }
             Console.WriteLine("");
+        }
+
+        public static void test1(int[] arr)
+        {
+            int len = arr.Length;
+            int temp = 0;
+            for (int m = len/2; m >0; m/=2)
+            {
+            for (int i = m; i < len; i++)
+            {
+                for (int j =i- m; j >=0; j-= m)
+                {
+                    if (arr[j] > arr[j + m])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + m];
+                        arr[j + m] = temp;
+                    }
+                }
+            }
+            }
+        }
+
+        public static void test2(int[] arr)
+        {
+            int len = arr.Length;//取数组长度
+            int temp = 0;
+            for (int m = len / 2; m > 0; m /= 2)
+            {
+                for (int i = m; i < len; i++)
+                {
+                    int j = i;
+                    temp = arr[j];
+                    if (arr[j]<arr[j-m])
+                    {
+                        while (j-m>=0&&temp<arr[j-m])
+                        {
+                            arr[j] = arr[j - m];
+                            j -= m;
+                        }
+                        arr[j] = temp;
+                    }
+                }
+            }
         }
     }
 }
---
+++ 数据结构-快速排序/demo/demo/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {-9,78,0,23,-567,70,-1,900,4568 };
+            quickSort(arr,0,arr.Length-1);
+            print(arr);
+            Console.ReadKey();
+        }
+        public static void quickSort(int[] arr,int left,int right)
+        {
+            int l = left;//左下标
+            int r = right;//右下标
+            int temp = 0;
+            //prvot 中轴值
+            int prvot = arr[(l + r) / 2];
+            //while循环的目的是让比prvot值小的左，比它大的放右边
+            while (l<r)
+            {
+                //在prvot左边一直找，找到大于等于provt值，才推出
+                while (arr[l]<prvot)
+                {
+                    l += 1;
+                }
+                //在prvot右边一直找，找到大于等于provt值，才推出
+                while (arr[r] > prvot)
+                {
+                    r -= 1;
+                }
+                //[如果条件成立，prvot的左右两的值，已经按照左边全部是]
+                if (l>=r)
+                {
+                    break;
+                }
+                //交换
+                temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+                //如果交换完，arr[l]==pivot值相等，前移
+                if (arr[l]==prvot)
+                {
+                    r -= 1;
+                }
+                //如果交换完，arr[r]==pivot值相等，后移
+                if (arr[r] == prvot)
+                {
+                    l += 1;
+                }
+            }
+            //如果l==r,必须l++,r--，否则会出现栈溢出
+            if (l==r)
+            {
+                l+=1;
+                r-=1;
+            }
+            //向左递归
+            if (left<r)
+            {
+                quickSort(arr,left,r);
+            }
+            //向左递归
+            if (right >l)
+            {
+                quickSort(arr, l, right);
+            }
+        }
+
+        public static void print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
